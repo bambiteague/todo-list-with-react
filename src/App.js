fb2a5fb0924246837bf907763b50a954858944e6
bambi teague <teeegsa@gmail.com>
2021-12-02 16:10:46 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
+
 //importing components-->
-import Form from "./components/Form"
+import Form from "./components/Form.js";
+import TodoList from "./components/TodoList.js";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all")
+  const [filteredTodos, setFilteredTodos] = useState([]); //in progress with filters portion of the app
   return (
     <div className="App">
       <header>
         <h1>Bambi's Todo List</h1>
       </header>
-        <Form />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+        setStatus={setStatus}
+
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos} 
+      />
     </div>
   );
 }
